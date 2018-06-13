4f78da594cb40ab33f439349ed78b1ff70d6f2c3
Jerry Hagen <jerry.hagen@cgi.com>
2018-06-13 17:50:01 -0500

    placements hb template

--- src/main/resources/static/js/portalweb.js
+++ src/main/resources/static/js/portalweb.js
@@ -1,10 +1,31 @@
+var handlebars_placementsTemplate;
+
 function loadProvider() {
+	
+	handlebars_placementsTemplate = Handlebars.compile($('#placementsTemplate').html());
+	
+	registerHandlebarsHelpers();
 	
 	var idprvdorg = 9000000; // get dynamically
 	
 	loadProviderPlacements(idprvdorg);
 
 
+}
+
+function registerHandlebarsHelpers() {
+	
+	Handlebars.registerHelper('formatDateSlashes', function(text, url) {
+		  text = Handlebars.Utils.escapeExpression(text);
+		  url  = Handlebars.Utils.escapeExpression(url);
+
+		  var year = text.substring(0,4);
+		  var month = parseInt(text.substring(5,7));
+		  var day = parseInt(text.substring(8,10));
+
+		  return new Handlebars.SafeString(month + "/" + day + "/" + year);
+	});
+	
 }
 
 function loadProviderPlacements(idprvdorg) {
@@ -17,8 +38,9 @@ function loadProviderPlacements(idprvdorg) {
         success: function (result) {
         	
         	var data = JSON.stringify(result);
+        	//$('#divChildrenInPlacement').html(data);
         	
-        	$('#divChildrenInPlacement').html(data);
+        	$('#divChildrenInPlacement').html(handlebars_placementsTemplate(result));
         },
         error: function () {
         	$('#divChildrenInPlacement').html("An error occurred trying to access the endpoint " + 'placements/episodesByProvider/' + idprvdorg);
