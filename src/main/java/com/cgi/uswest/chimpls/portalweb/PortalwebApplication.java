a5e2d94064fb7c73f1870ab345b65056bc4d6b7a
Jerry Hagen <jerry.hagen@cgi.com>
2018-06-24 21:14:42 -0500

    Google OAuth2 SSO

--- src/main/java/com/cgi/uswest/chimpls/portalweb/PortalwebApplication.java
+++ src/main/java/com/cgi/uswest/chimpls/portalweb/PortalwebApplication.java
@@ -2,13 +2,18 @@ package com.cgi.uswest.chimpls.portalweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableOAuth2Sso
 public class PortalwebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortalwebApplication.class, args);
 	}
+	
 }
